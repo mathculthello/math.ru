8f21ec64ccacb6955a2592f2db76204593c0b400
Egor Kuzmichev <egor@kuzmichev.design>
2019-02-11 13:45:03 +0300

    Введены контроллеры

---
+++ controllers/problems/index.php
@@ -0,0 +1,4 @@
+<?php
+require_once 'menu.inc.php';
+$_SMARTY->display('problems/index.tpl');
+?>
--- public/index.php
+++ public/index.php
@@ -1,29 +1,31 @@
 <?php
 require_once 'set_env.inc.php';
 
-$sql = 'SELECT a.id,a.txt,a.page,a.shift,a.book_id,b.title,'.
-    '(CASE WHEN b.djvu_file!=\'\' THEN b.djvu_file WHEN b.pdf_file!=\'\' THEN b.pdf_file WHEN b.ps_file!=\'\' THEN b.ps_file WHEN b.tex_file!=\'\' THEN b.tex_file WHEN b.html_file!=\'\' THEN b.html_file ELSE b.id END) AS path '.
-    'FROM lib_ad a, lib_book b WHERE b.id=a.book_id ORDER BY RAND() LIMIT 1';
-$ad = $_PDO->query($sql);
-$ad = $ad->fetch();
+/* 
+ * ROUTING
+ */
 
-$sql = 'SELECT p.fname,p.sname,p.lname FROM h_person p,lib_b2a b WHERE b.book=:book_id AND p.id=b.author';
+use Symfony\Component\Routing\Route;
+use Symfony\Component\Routing\RouteCollection;
+use Symfony\Component\Routing\RequestContext;
+use Symfony\Component\Routing\Matcher\UrlMatcher;
+use Symfony\Component\Config\FileLocator; 
+use Symfony\Component\Routing\Loader\YamlFileLoader;
 
-$sth=$_PDO->prepare($sql);
-$sth->execute(array(':book_id'=>$ad['book_id']));
-$ad['author']=$sth->fetchAll(); 
+$fileLocator = new FileLocator([APP_ROOT]); 
+$loader = new YamlFileLoader($fileLocator); 
+$routes = $loader->load('etc/routes.yaml');
 
-$ad['txt'] = replace_tex($ad['txt'], '/lib/ad/img/img_'.$ad['id'].'_');
-$_SMARTY->assign('ad', $ad);
+$context = new RequestContext('/');
+$matcher = new UrlMatcher($routes, $context);
+$current_uri = parse_url($_SERVER["REQUEST_URI"], PHP_URL_PATH);
 
-$_SMARTY->assign('_menu', array(
-		array('path' => '/lib/', 'title' => 'Библиотека'),
-		array('path' => '/media/', 'title' => 'Медиатека'),
-		array('path' => 'http://olimpiada.ru', 'title' => 'Олимпиады'),
-		array('path' => '/problems/', 'title' => 'Задачи'),
-		array('path' => '/schools/', 'title' => 'Научные школы'),
-		array('path' => '/teacher/', 'title' => 'Учительская'),
-		array('path' => '/history/', 'title' => 'История математики'),
-		array('path' => '/founders/', 'title' => 'Учредители и спонсоры')));
-$_SMARTY->display('index.tpl');
-?>
+try {
+	$parameters = $matcher->match($current_uri);
+}
+catch(Exception $e) {
+	include '404.php';
+	die();
+}
+
+include ('controllers/'.$parameters['_controller']);
