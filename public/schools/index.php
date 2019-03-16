74dd212a0421539d182ea4250e430c6eb44bad1d
Egor Kuzmichev <yegor.kuzmichev@gmail.com>
2019-03-16 19:09:48 +0300

    removed controllers

--- public/schools/index.php
+++ public/schools/index.php
@@ -1,4 +1,5 @@
 <?php
+require_once '../set_env.inc.php';
 require_once 'menu.inc.php';
 $_SMARTY->assign('nav_bar', array(array('url' => '/', 'name' => 'MATH.RU'), array('url' => '', 'name' => 'Выездные научные школы')));
 $_SMARTY->display('schools/index.tpl');
