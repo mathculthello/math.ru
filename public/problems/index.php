b165aac23e82a465410ad29d5ba37a08c46d1e63
Egor Kuzmichev <egor@kuzmichev.design>
2019-03-16 19:12:58 +0300

    include fix

--- public/problems/index.php
+++ public/problems/index.php
@@ -1,5 +1,5 @@
 <?php
 require_once '../set_env.inc.php';
-require_once 'menu.inc.php';
+require_once '../menu.inc.php';
 $_SMARTY->display('problems/index.tpl');
 ?>
--- public/schools/index.php
+++ public/schools/index.php
@@ -1,6 +1,6 @@
 <?php
 require_once '../set_env.inc.php';
-require_once 'menu.inc.php';
+require_once '../menu.inc.php';
 $_SMARTY->assign('nav_bar', array(array('url' => '/', 'name' => 'MATH.RU'), array('url' => '', 'name' => 'Выездные научные школы')));
 $_SMARTY->display('schools/index.tpl');
 ?>
