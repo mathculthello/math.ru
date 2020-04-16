22495d9051df35b027156b018557f9624e8501bf
Egor Kuzmichev <egor@kuzmichev.design>
2020-04-16 11:46:57 +0300

    security fix

--- public/history/stories/story.php
+++ public/history/stories/story.php
@@ -1,7 +1,7 @@
 <?php
 require_once '../../set_env.inc.php';
 require_once '../menu.inc.php';
-$id = $_GET['id'];
+$id = (int) $_GET['id'];
 if(empty($id)) exit;
 $sql = "SELECT id,date,title,REPLACE(text,'@@br@@','') AS text,source FROM h_story WHERE id=".$id;
 $story = $_ADODB->GetRow($sql);
